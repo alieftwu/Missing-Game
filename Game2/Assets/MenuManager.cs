686bcb2a0184143f1d7c947c2dde8a6a8ee5127f
CYB101NAME <keshawndavis472@gmail.com>
2024-03-18 01:30:14 -0500

    Moved custom font file

--- Game2/Assets/MenuManager.cs
+++ Game2/Assets/MenuManager.cs
@@ -8,7 +8,7 @@ public class MainMenu: MonoBehaviour {
 
     public void PlayGame() {
         SceneManager.LoadScene("Floor 1");
-        Time.timeScale = 0;
+
 
     }
     public void QuitGame() {
